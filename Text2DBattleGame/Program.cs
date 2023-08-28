fe7b3da3e5e02bb2832a7b139e373864f37a9514
juyonLee00 <ljy012900@gmail.com>
2023-08-28 11:30:14 +0900

    [feat] 개발 환경 세팅

---
+++ Text2DBattleGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Text2DBattleGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
