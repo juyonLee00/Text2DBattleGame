33564465859a97b300d70939ae064ec693f96b90
Lyrwhitt <47979697+Lyrwhitt@users.noreply.github.com>
2023-08-28 14:02:18 +0900

    '

--- Text2DBattleGame/Program.cs
+++ Text2DBattleGame/Program.cs
@@ -1,114 +1,139 @@
-﻿using System;
-
-namespace Text2DBattleGame
+public enum Scene
 {
-    internal class Program
+    None, GameIntro, MyInfo
+}
+
+internal class Program
+{
+    private static Scene scene = Scene.None;
+
+    private static Character player;
+
+    static void Main(string[] args)
     {
-        private static Character player;
+        GameDataSetting();
+        DisplayGameIntro();
+    }
 
-        static void Main(string[] args)
+    static void Update()
+    {
+        while(true)
         {
-            GameDataSetting();
-            DisplayGameIntro();
-        }
-
-        static void GameDataSetting()
-        {
-            // 캐릭터 정보 세팅
-            player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
-
-            // 아이템 정보 세팅
-        }
-
-        static void DisplayGameIntro()
-        {
-            Console.Clear();
-
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-            Console.WriteLine();
-            Console.WriteLine("1. 상태 보기");
-            Console.WriteLine("2. 전투 시작");
-            Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-
-            int input = CheckValidInput(1, 2);
-            switch (input)
+            switch (scene)
             {
-                case 1:
-                    DisplayMyInfo();
-                    break;
-
-                case 2:
-                    DisplayBattleStart();
-                    break;
-            }
-        }
-
-        static void DisplayMyInfo()
-        {
-            Console.Clear();
-
-            Console.WriteLine("상태보기");
-            Console.WriteLine("캐릭터의 정보르 표시합니다.");
-            Console.WriteLine();
-            Console.WriteLine($"Lv.{player.Level}");
-            Console.WriteLine($"{player.Name}({player.Job})");
-            Console.WriteLine($"공격력 :{player.Atk}");
-            Console.WriteLine($"방어력 : {player.Def}");
-            Console.WriteLine($"체력 : {player.Hp}");
-            Console.WriteLine($"Gold : {player.Gold} G");
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-
-            int input = CheckValidInput(0, 0);
-            switch (input)
-            {
-                case 0:
+                case Scene.None:
                     DisplayGameIntro();
                     break;
-            }
-        }
-
-        static void DisplayInventory()
-        {
-
-        }
-
-        static void DisplayBattleStart()
-        {
-            Console.Clear();
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Battle!!\n");
-
-            Random random = new Random();
-            int howMany = random.Next(0, 5);
-            Monster[] battleMonsters = new Monster[howMany];
-            for(int i = 0; i < howMany; i++)
-            {
-
-            }
-        }
-
-        static int CheckValidInput(int min, int max)
-        {
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                bool parseSuccess = int.TryParse(input, out var ret);
-                if (parseSuccess)
-                {
-                    if (ret >= min && ret <= max)
-                        return ret;
-                }
-
-                Console.WriteLine("잘못된 입력입니다.");
+                case Scene.GameIntro:
+                    DisplayGameIntro();
+                    break;
+                case Scene.MyInfo:
+                    DisplayMyInfo();
+                    break;
             }
         }
     }
 
+    static void GameDataSetting()
+    {
+        // 캐릭터 정보 세팅
+        player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+
+        // 아이템 정보 세팅
+    }
+
+    static void DisplayGameIntro()
+    {
+        Console.Clear();
+
+        Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+        Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
+        Console.WriteLine();
+        Console.WriteLine("1. 상태보기");
+        Console.WriteLine("2. 인벤토리");
+        Console.WriteLine();
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+        int input = CheckValidInput(1, 2);
+        switch (input)
+        {
+            case 1:
+                DisplayMyInfo();
+                break;
+
+            case 2:
+                // 작업해보기
+                break;
+        }
+    }
+
+    static void DisplayMyInfo()
+    {
+        Console.Clear();
+
+        Console.WriteLine("상태보기");
+        Console.WriteLine("캐릭터의 정보르 표시합니다.");
+        Console.WriteLine();
+        Console.WriteLine($"Lv.{player.Level}");
+        Console.WriteLine($"{player.Name}({player.Job})");
+        Console.WriteLine($"공격력 :{player.Atk}");
+        Console.WriteLine($"방어력 : {player.Def}");
+        Console.WriteLine($"체력 : {player.Hp}");
+        Console.WriteLine($"Gold : {player.Gold} G");
+        Console.WriteLine();
+        Console.WriteLine("0. 나가기");
+
+        int input = CheckValidInput(0, 0);
+        switch (input)
+        {
+            case 0:
+                DisplayGameIntro();
+                break;
+        }
+    }
+
+    static void DisplayInventory()
+    {
+
+    }
+
+    static int CheckValidInput(int min, int max)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            bool parseSuccess = int.TryParse(input, out var ret);
+            if (parseSuccess)
+            {
+                if (ret >= min && ret <= max)
+                    return ret;
+            }
+
+            Console.WriteLine("잘못된 입력입니다.");
+        }
+    }
+}
 
 
+public class Character
+{
+    public string Name { get; }
+    public string Job { get; }
+    public int Level { get; }
+    public int Atk { get; }
+    public int Def { get; }
+    public int Hp { get; }
+    public int Gold { get; }
+
+    public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+    {
+        Name = name;
+        Job = job;
+        Level = level;
+        Atk = atk;
+        Def = def;
+        Hp = hp;
+        Gold = gold;
+    }
 }
