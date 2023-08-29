2caaf7559c11eab50ef495314159fce015c79460
박민혁 <129154514+plumas90@users.noreply.github.com>
2023-08-29 14:56:39 +0900

    [feat]아이템창 추가
    장착 미구현 이므로 무반응 합니다

---
+++ Text2DBattleGame/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text2DBattleGame
+{
+    internal class Inventory
+    {
+            public static void DisplayInventory(Character player)
+            {
+                Console.Clear();
+
+                Console.WriteLine("[인벤토리]");
+                Console.WriteLine("");
+                Console.WriteLine("아이템 목록");
+
+                Console.WriteLine("0. 나가기");
+                int i = 1;
+                foreach (Item item in player.Inventory) 
+                {
+                    Console.WriteLine($"{i}. {item.Name}");
+                     ++i;
+                }
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            int input = Program.CheckValidInput(0,i);
+                switch (input)
+                {
+                    case 0:
+                    Program.scene= Scene.GameIntro;
+                    break;
+
+                    default:
+                    // 사용 함수
+                    Program.scene = Scene.GameIntro;
+                    break;
+                }
+            }
+                  
+    }
+}
