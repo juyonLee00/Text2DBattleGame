39203f11226d95d16633211e5e9d5f0b6f15d0d1
juyonLee00 <ljy012900@gmail.com>
2023-08-31 12:15:20 +0900

    [fix] 상태창에 아이템 교체 반영되지 않던 부분 수정

--- Text2DBattleGame/Inventory.cs
+++ Text2DBattleGame/Inventory.cs
@@ -17,6 +17,14 @@ namespace Text2DBattleGame
             Console.WriteLine("아이템 목록");
             int i = 1;
 
+            AttackItem item1 = new AttackItem("스파르타의 창", "아주 오래된 검입니다.", false, 2, 0, 0, 0, 'a');
+            DefenseItem item2 = new DefenseItem("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", false, 0, 5, 0, 0, 'd');
+            AttackItem item3 = new AttackItem("청동 도끼", "아주 오래된 도끼입니다.", false, 7, 0, 0, 0, 'a');
+
+            player.Inventory.Add(item1);
+            player.Inventory.Add(item2);
+            player.Inventory.Add(item3);
+
             foreach (IItem item in player.Inventory)
             {
                 Console.Write($"{i}. ");
@@ -29,11 +37,11 @@ namespace Text2DBattleGame
                 Console.Write($"{item.Name}\n");
                 ++i;
             }
-
+            
             Console.WriteLine("\n0. 나가기");
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             Console.Write(">> ");
-
+            
             int input = Program.CheckValidInput(0, i);
             switch (input)
             {
